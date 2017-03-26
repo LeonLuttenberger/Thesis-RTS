fb3257aff3f52a011b3c33ef7cf76aced1fc0103
Leon Luttenberger <luttenberger.leon@gmail.com>
2017-03-26 16:18:00 +0200

    Generated libGDX project

---
+++ desktop/src/hr/fer/zemris/zavrsni/rts/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hr.fer.zemris.zavrsni.rts.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hr.fer.zemris.zavrsni.rts.MyRTS;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyRTS(), config);
+	}
+}
