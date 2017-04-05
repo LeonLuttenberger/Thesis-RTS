b15515ff927a40f70cb1849cc8fd5e9585445740
Leon Luttenberger <luttenberger.leon@gmail.com>
2017-04-05 22:14:39 +0200

    Added the texture packer dependency

--- desktop/src/hr/fer/zemris/zavrsni/rts/desktop/DesktopLauncher.java
+++ desktop/src/hr/fer/zemris/zavrsni/rts/desktop/DesktopLauncher.java
@@ -2,11 +2,35 @@ package hr.fer.zemris.zavrsni.rts.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import hr.fer.zemris.zavrsni.rts.MyRTS;
 
 public class DesktopLauncher {
+
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = false;
+
 	public static void main (String[] arg) {
+		if (rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+
+			TexturePacker.processIfModified(
+					settings,
+					"../assets-raw/images",
+					"images",
+					"textures"
+			);
+		}
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "My RTS";
+
 		new LwjglApplication(new MyRTS(), config);
 	}
 }
