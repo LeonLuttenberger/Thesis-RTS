2ca629d300506a6a7f99a02a6310632a589cabc7
Leon Luttenberger <luttenberger.leon@gmail.com>
2017-05-27 09:46:57 +0200

    Tweaked the bullet speed

--- core/src/hr/fer/zemris/zavrsni/rts/objects/projectiles/AbstractBullet.java
+++ core/src/hr/fer/zemris/zavrsni/rts/objects/projectiles/AbstractBullet.java
@@ -10,7 +10,7 @@ public abstract class AbstractBullet extends Projectile {
 
     private static final int WIDTH = 5;
     private static final int HEIGHT = 5;
-    private static final float MAX_SPEED = 500f;
+    private static final float MAX_SPEED = 8000f;
 
     public AbstractBullet(TextureRegion region, ILevel level, IRangedAttacker<? extends AbstractGameObject> source,
                   IDamageable<? extends AbstractGameObject> target, int attackPower) {
